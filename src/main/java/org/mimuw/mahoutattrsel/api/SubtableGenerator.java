8567e0a09b1757bc93d07a638be8c9c0a10ce067
Paweł <cpawols@gmail.com>
2014-11-25 15:03:10 +0100

    test commit

--- src/main/java/org/mimuw/mahoutattrsel/api/SubtableGenerator.java
+++ src/main/java/org/mimuw/mahoutattrsel/api/SubtableGenerator.java
@@ -2,6 +2,9 @@ package org.mimuw.mahoutattrsel.api;
 
 import java.util.List;
 
+/**
+ * This is an interface responsible for subtable generation.
+ */
 public interface SubtableGenerator<T> {
 
     List<T> getSubtables();
