b5d9f5bdbc21fcd03af83f17ae6bc0f2dc88ff62
Paweł <cpawols@gmail.com>
2014-11-29 00:05:41 +0100

    MockitoTest to ObjectGenerator

--- src/test/java/org/mimuw/mahoutattrsel/MatrixFixedSizeObjectSubtableGeneratorTest.java
+++ src/test/java/org/mimuw/mahoutattrsel/MatrixFixedSizeObjectSubtableGeneratorTest.java
@@ -7,7 +7,9 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.Random;
 
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MatrixFixedSizeObjectSubtableGeneratorTest {
 
@@ -44,7 +46,56 @@ public class MatrixFixedSizeObjectSubtableGeneratorTest {
     @Test
     public void testMockitogetSubtables() throws Exception {
 
-        MatrixFixedSizeObjectSubtableGenerator test = mock(MatrixFixedSizeObjectSubtableGenerator.class);
+        Random random = mock(Random.class);
+
+        when(random.nextInt(anyInt())).thenReturn(0, 1);
+
+        MatrixFixedSizeObjectSubtableGenerator matrix = new MatrixFixedSizeObjectSubtableGenerator(random, 1, 2,
+                new DenseMatrix(new double[][]{{1, 2, 3}, {7, 6, 5}}));
+
+        List<Matrix> listOfSubtables = matrix.getSubtables();
+
+        MatrixAssert.assertThat(listOfSubtables.get(0)).isEqualTo(new DenseMatrix(new double[][]{{1, 2, 3}, {7, 6, 5}}));
+
+    }
+
+    @Test
+    public void testTwoMockitogetSubtables() throws Exception {
+
+        Random random = mock(Random.class);
+
+        when(random.nextInt(anyInt())).thenReturn(0, 1, 2);
+        MatrixFixedSizeObjectSubtableGenerator matrixUnderTest = new MatrixFixedSizeObjectSubtableGenerator(random, 1, 3,
+                new DenseMatrix(new double[][]{{1, 2}, {3, 4}, {5, 6}, {7, 6}, {8, 9}}));
+
+        List<Matrix> listOfSubtables = matrixUnderTest.getSubtables();
+
+
+        MatrixAssert.assertThat(listOfSubtables.get(0)).isEqualTo(
+                new DenseMatrix(new double[][]{{1, 2}, {3, 4}, {5, 6}}));
+
+
+    }
+
+    @Test
+    public void testThreeMockitogetSubtables() throws Exception {
+
+        Random random = mock(Random.class);
+
+        when(random.nextInt(anyInt())).thenReturn(0, 1, 2, 3, 0, 1, 2, 4, 4, 2, 1, 0);
+        MatrixFixedSizeObjectSubtableGenerator matrixUnderTest = new MatrixFixedSizeObjectSubtableGenerator(random, 3, 4,
+                new DenseMatrix(new double[][]{{1, 2}, {3, 4}, {5, 6}, {7, 6}, {8, 9}}));
+
+        List<Matrix> listOfSubtables = matrixUnderTest.getSubtables();
+
+        MatrixAssert.assertThat(listOfSubtables.get(0)).isEqualTo(
+                new DenseMatrix(new double[][]{{1, 2}, {3, 4}, {5, 6}, {7, 6}}));
+
+        MatrixAssert.assertThat(listOfSubtables.get(1)).isEqualTo(
+                new DenseMatrix(new double[][]{{1, 2}, {3, 4}, {5, 6}, {8, 9}}));
+
+        MatrixAssert.assertThat(listOfSubtables.get(2)).isEqualTo(
+                new DenseMatrix(new double[][]{{1, 2}, {3, 4}, {5, 6}, {8, 9}}));
 
 
     }
