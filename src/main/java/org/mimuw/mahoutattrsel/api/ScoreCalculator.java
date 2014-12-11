3c6850378a42c5d8e6a1106913065cfbf9a8d764
wiktorg <w.gromniak@students.mimuw.edu.pl>
2014-12-11 16:18:14 +0100

    Updated ScoreCalculator.

--- src/main/java/org/mimuw/mahoutattrsel/api/ScoreCalculator.java
+++ src/main/java/org/mimuw/mahoutattrsel/api/ScoreCalculator.java
@@ -1,12 +1,10 @@
 package org.mimuw.mahoutattrsel.api;
 
 /**
- * The implementations should be instantiated with the additional (apart from attribute id) information they need to
- * calculate the score.
- *
- * @param <T> type of attribute id; mostly {@link String} or {@link Integer}
+ * A single instance of ScoreCalculator calculates score for a fixed attribute. The implementations should be
+ * instantiated with additional information they need to calculate the score.
  */
-public interface ScoreCalculator<T> {
+public interface ScoreCalculator {
 
-    double getScore(T attributeId);
+    double getScore();
 }
